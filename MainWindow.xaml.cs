ee09ecbbf0eb3f52898cd1b2007f13bf93e32d28
Ketrin <ketrin632@gmail.com>
2020-12-01 00:07:43 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace DocumentEditor
+{
+    internal sealed partial class MainWindow : Window
+    {
+        TextPointer startIndexPointer, endIndexPointer;
+        int startIndex=0, endIndex=0;
+        public MainWindow()
+        {
+            InitializeComponent();           
+
+        }
+        private void boldButton_Click(object sender, RoutedEventArgs e)
+        {
+            GetSelectionText().ApplyPropertyValue(Run.FontWeightProperty, FontWeights.Bold);
+        }
+
+        private void italicButton_Click(object sender, RoutedEventArgs e)
+        {
+            GetSelectionText().ApplyPropertyValue(Run.FontStyleProperty, FontStyles.Italic);
+        }
+
+        private void underlineButton_Click(object sender, RoutedEventArgs e)
+        {
+            GetSelectionText().ApplyPropertyValue(Run.TextDecorationsProperty, TextDecorations.Underline);
+        }
+
+        private TextSelection GetSelectionText()
+        {
+            startIndexPointer = richTextBox.GetTextPointerAtOffset(startIndex);
+            endIndexPointer = richTextBox.GetTextPointerAtOffset(endIndex);
+            richTextBox.Selection.Select(startIndexPointer, endIndexPointer);
+            return richTextBox.Selection;
+        }
+
+        private void fontSizeComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (textRun != null && fontSizeComboBox.SelectedIndex!=-1)
+            {
+                double size = double.Parse(((ComboBoxItem)fontSizeComboBox.SelectedItem).Content.ToString());
+                GetSelectionText().ApplyPropertyValue(Run.FontSizeProperty, size);
+            }
+        }
+
+        private void colorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (textRun != null && colorComboBox.SelectedIndex != -1)
+            {
+                Color color=Colors.White;
+                switch (colorComboBox.SelectedIndex)
+                {
+                    case 0:
+                        color =Colors.Black;
+                        break;
+                    case 1:
+                        color = Colors.Blue;
+                        break;
+                    case 2:
+                        color = Colors.Green;
+                        break;
+                    case 3:
+                        color = Colors.Orange;
+                        break;
+                    case 4:
+                        color = Colors.Purple;
+                        break;
+                    case 5:
+                        color = Colors.Red;
+                        break;
+                    case 6:
+                        color = Colors.Yellow;
+                        break;
+                    default:
+                        break;
+                }
+
+                GetSelectionText().ApplyPropertyValue(Run.ForegroundProperty, new SolidColorBrush(color));
+            }
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {    
+            GetSelectionText().ApplyPropertyValue(Run.FontWeightProperty, FontWeights.Normal);
+            GetSelectionText().ApplyPropertyValue(Run.FontStyleProperty, FontStyles.Normal);
+            GetSelectionText().ApplyPropertyValue(Run.TextDecorationsProperty, null);
+            GetSelectionText().ApplyPropertyValue(RichTextBox.FontSizeProperty, 12.0);
+            GetSelectionText().ApplyPropertyValue(Run.ForegroundProperty, new SolidColorBrush(Colors.Black));
+        }
+
+        private void startTexBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (textRun == null)
+                return;
+
+            startIndex = int.Parse(startTexBox.Text);
+            endIndex = int.Parse(endTexBox.Text);
+
+            if (startIndex <= endIndex && endIndex < textRun.Text.Length && startIndex >= 0 && endIndex >= 0)
+            {
+                //endIndex++;
+                ActivateOrDeactivate(true);
+            }
+            else
+            {
+                ActivateOrDeactivate(false);
+            }
+        }
+
+        private void ActivateOrDeactivate(bool tb)
+        {
+            toolBar1.IsEnabled = tb;
+            toolBar2.IsEnabled = tb;
+            toolBar3.IsEnabled = tb;
+        }
+        
+    }
+}
---
+++ SomeClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace DocumentEditor
+{
+    static public class SomeClass
+    {
+        public static TextPointer GetTextPointerAtOffset(this RichTextBox richTextBox, int offset)
+        {
+            var navigator = richTextBox.Document.ContentStart;
+            int cnt = 0;
+
+            while (navigator.CompareTo(richTextBox.Document.ContentEnd) < 0)
+            {
+                switch (navigator.GetPointerContext(LogicalDirection.Forward))
+                {
+                    case TextPointerContext.ElementStart:
+                        break;
+                    case TextPointerContext.ElementEnd:
+                        if (navigator.GetAdjacentElement(LogicalDirection.Forward) is Paragraph)
+                            cnt += 2;
+                        break;
+                    case TextPointerContext.EmbeddedElement:
+                        cnt++;
+                        break;
+                    case TextPointerContext.Text:
+                        int runLength = navigator.GetTextRunLength(LogicalDirection.Forward);
+
+                        if (runLength > 0 && runLength + cnt < offset)
+                        {
+                            cnt += runLength;
+                            navigator = navigator.GetPositionAtOffset(runLength);
+                            if (cnt > offset)
+                                break;
+                            continue;
+                        }
+                        cnt++;
+                        break;
+                }
+
+                if (cnt > offset)
+                    break;
+
+                navigator = navigator.GetPositionAtOffset(1, LogicalDirection.Forward);
+
+            }
+
+            return navigator;
+        }
+    }
+}
